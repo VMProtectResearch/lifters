b6920e4a62dfc8fdf160d34d21b1a448da31fa64
极诣·剑魂 <84119280+helloobaby@users.noreply.github.com>
2022-03-26 14:16:31 +0800

    update

--- llvm/lifters.hpp
+++ llvm/lifters.hpp
@@ -1,6 +1,8 @@
 #pragma once 
 
 #include <vmprofiles.hpp>
+#include <memory>
+#include <variant>
 
 #include <llvm/IR/BasicBlock.h>
 #include <llvm/IR/Constants.h>
@@ -19,19 +21,37 @@
 #include <llvm/IR/LegacyPassManager.h>
 #include <llvm/Passes/PassBuilder.h>
 
+using namespace llvm;
+
+using handler_function_type = void(BasicBlock, IRBuilder<>, std::variant<uint64_t, uint32_t, uint16_t, uint8_t>, std::variant<uint64_t, uint32_t, uint16_t, uint8_t>, std::variant<uint64_t, uint32_t, uint16_t, uint8_t>);
 
 namespace lifters {
+	class _cvmp2
+	{
+	public:
+		_cvmp2(LLVMContext& context, IRBuilder<>& builder, Module* llvm_module) :context(context), builder(builder), llvm_module(llvm_module) {
+
+		}
+
+		LLVMContext& context;
+		IRBuilder<>& builder;
+		std::unique_ptr<Module> llvm_module;
+
+	};
 
 	struct lifters
 	{
 		vm::handler::mnemonic_t mnemonic;  //用来定位一个vm handler对应的lifter
 
-		std::function<void(llvm::BasicBlock,llvm::IRBuilder<>,uint64_t,uint64_t,uint64_t)> _f;   //处理basic block
-
+		//void func(BasicBlock, IRBuilder<>,uint64_t param1,uint64_t param2,uint64_t param3)
+		std::function<handler_function_type> _f;
 
 	};
 
-
+	std::map<vm::handler::mnemonic_t, lifters> _h_map
+	{
+		{}
+	}
 	
 
 }
